e8fdc4d35e8b5506da0bfd657de1b28794a70014
Sav Younts <savyounts@gmail.com>
2018-08-22 22:04:01 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,14 @@
 require 'pry'
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   @@all = []
 
-  def initialize (id:, name:, type:, db:)
+  def initialize (id:, name:, type:, db:, hp:)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = nil
     @@all << self
   end
 
@@ -18,5 +19,10 @@ class Pokemon
   def self.find(id, db)
     result = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2], db: db)
+  end
+
+  def alter_hp(hp)
+    
+
   end
 end
